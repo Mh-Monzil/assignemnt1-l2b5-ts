37ae2bea924361446dcfc5288339192a556c67e8
Mh-Monzil <monzil246@gmail.com>
2025-05-07 20:39:40 +0600

    done 3

--- index.ts
+++ index.ts
@@ -10,3 +10,7 @@ type Book = {
 function filterByRating(items: Book[]): Book[] {
   return items.filter((item) => item.rating >= 4);
 }
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.flat();
+}
